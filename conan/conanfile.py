193c92624977e38355e5e02915eda90c11cc9215
wducharme <wesleyducharme1997@gmail.com>
2022-11-07 23:15:25 -0500

    Using conan 2.0 beta

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -1,16 +1,21 @@
 from conan import ConanFile
 from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
+from conan.tools.files import load, copy
+import re
 
 
 class NerdfontCodepointsConan(ConanFile):
     name = "nerdfont-codepoints"
-    version = "0.0.1"
     settings = "os", "compiler", "build_type", "arch"
 
+    def set_version(self):
+        content = load(self, "CMakeLists.txt")
+        self.version = re.search(rf"set\({self.name}_VERSION (.*)\)", content).group(1)
+
     def export_sources(self):
-        self.copy("../CMakeLists.txt")
-        self.copy("*.h", "include", "../include")
-        self.copy("../cmake/config.cmake.in", "cmake")
+        copy(self, "CMakeLists.txt", f"{self.recipe_folder}/../", self.export_sources_folder)
+        copy(self, "*.h", f"{self.recipe_folder}/../include", f"{self.export_sources_folder}/include")
+        copy(self, "*", f"{self.recipe_folder}/../cmake", f"{self.export_sources_folder}/cmake")
 
     def layout(self):
         cmake_layout(self)
--- conan/setup.ps1
+++ conan/setup.ps1
@@ -16,5 +16,7 @@ Invoke-Expression "$VenvPython -m pip install -r ./requirements.txt"
 
 # Will fail to start process when we have python or the venv in a path with spaces
 $VenvConan = "$VenvPath/Scripts/conan.exe"
+
+# Will produce an error when the remote is already added. Happens when the user cache already had the remote added.
+# We could try to set the environment variable CONAN_USER_HOME to change this to be local to the repository and clear it in this script. Avoiding it for simplicity.
 Invoke-Expression "$VenvConan remote add wducharme-cpp-packages https://wducharme.jfrog.io/artifactory/api/conan/cpp-packages"
-# if we needed user auth we would do something like: conan user -p <pass> -r wducharme-cpp-packages <email>
