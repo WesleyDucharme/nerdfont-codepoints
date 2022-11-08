d1417cbbca3eed329d7830beba05985aa1b45d38
wducharme <wesleyducharme1997@gmail.com>
2022-11-07 20:28:37 -0500

    Changes

--- conan/setup.ps1
+++ conan/setup.ps1
@@ -14,7 +14,7 @@ $VenvPython = "$VenvPath/Scripts/python.exe"
 Invoke-Expression "$VenvPython -m pip install --upgrade pip"
 Invoke-Expression "$VenvPython -m pip install -r ./requirements.txt"
 
-# Will fail to start process when we have python or the venv in a path with spaces...
+# Will fail to start process when we have python or the venv in a path with spaces
 $VenvConan = "$VenvPath/Scripts/conan.exe"
 Invoke-Expression "$VenvConan remote add wducharme-cpp-packages https://wducharme.jfrog.io/artifactory/api/conan/cpp-packages"
 # if we needed user auth we would do something like: conan user -p <pass> -r wducharme-cpp-packages <email>
