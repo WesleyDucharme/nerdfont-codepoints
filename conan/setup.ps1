9c77e8f4ba4c641751f484daa795fc19d197c0fe
wducharme <wesleyducharme1997@gmail.com>
2022-11-10 00:07:57 -0500

    Downgrading conan for stability

--- conan/setup.ps1
+++ conan/setup.ps1
@@ -14,9 +14,11 @@ $VenvPython = "$VenvPath/Scripts/python.exe"
 Invoke-Expression "$VenvPython -m pip install --upgrade pip"
 Invoke-Expression "$VenvPython -m pip install -r ./requirements.txt"
 
+# Because this library is standalone with no other conan dependencies we have no need to add the https://wducharme.jfrog.io/artifactory/api/conan/cpp-packages remote
+# It should be entirely up to CI/CD automation to add the remote, create the package and then upload.
+
 # Will fail to start process when we have python or the venv in a path with spaces
-$VenvConan = "$VenvPath/Scripts/conan.exe"
+# $VenvConan = "$VenvPath/Scripts/conan.exe"
 
 # Will produce an error when the remote is already added. Happens when the user cache already had the remote added.
-# We could try to set the environment variable CONAN_USER_HOME to change this to be local to the repository and clear it in this script. Avoiding it for simplicity.
-Invoke-Expression "$VenvConan remote add wducharme-cpp-packages https://wducharme.jfrog.io/artifactory/api/conan/cpp-packages"
+# Invoke-Expression "$VenvConan remote add wducharme-cpp-packages https://wducharme.jfrog.io/artifactory/api/conan/cpp-packages"
